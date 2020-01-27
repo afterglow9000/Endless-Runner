d67be75052788710eda9e7da239c7deedaf1e277
B. Bueno <afterglow9000@gmail.com>
2020-01-27 09:41:13 -0700

    added controls (Mobile Game In Unity Tutorial - Part 003)

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float gravity = 20.0f;
+    public float jumpSpeed = 20.5f;
+    public Vector3 moveDirection = Vector3.zero;
+    public float speed = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController characterController = GetComponent<CharacterController>();
+
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") + 3, 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y += jumpSpeed;
+            }
+        }
+        else
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y += jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1. [How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 001](https://youtu.be/Uyr8ErD_BIE)
 2. [How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 002 - Materials](https://www.youtube.com/watch?v=6AS4EkqzoKY)
+3. [How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 003 - Player](https://www.youtube.com/watch?v=Ej_U2ga9hRs)
