0b9ac07c1794cbf45842dffeb92db1dcd112b254
B. Bueno <afterglow9000@gmail.com>
2020-02-03 06:08:33 -0700

    created sta and planet objects (Mobile Game In Unity Tutorial - Part 005 )

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 1;
+        transform.Rotate(0, speed, 0, Space.World);
+    }
+}
