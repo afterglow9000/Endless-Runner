24eb889cd558884b47bbdd805e15443c26a73b9e
B. Bueno <afterglow9000@gmail.com>
2020-02-10 07:46:04 -0700

    created scoring system (Mobile Game In Unity Tutorial - Part 006)

---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public static int score;
+    public GameObject scoreDisplay;
+
+    void AddScore()
+    {
+        score += 5;
+        scoreDisplay.GetComponent<Text>().text = "Score: " + score;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("AddScore", 1, 0.1f);
+    }
+}
