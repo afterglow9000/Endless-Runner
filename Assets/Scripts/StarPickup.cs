136adab1d6c4a58cfc7895fa7010aaa3ebaa1026
B. Bueno <afterglow9000@gmail.com>
2020-02-17 09:22:01 -0700

    How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 007 - Fire & Score

---
+++ Assets/Scripts/StarPickup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class StarPickup : MonoBehaviour
+{
+    public int starScore = 500;
+    public AudioSource starSound;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        starSound.Play();
+        ScoringSystem.score += starScore;
+        this.transform.position = new Vector3(0, 1000, 0);
+    }
+}
