13b13dd579494dcfa81f8b6fd88ee73c8ae3f0bb
B. Bueno <afterglow9000@gmail.com>
2020-02-23 21:34:57 -0700

    How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 008 - Music

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject deathBlack;
+    public GameObject gameOver;
+    public GameObject uFO;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        deathBlack.SetActive(true);
+        gameOver.SetActive(true);
+        uFO.SetActive(false);
+    }
+}
