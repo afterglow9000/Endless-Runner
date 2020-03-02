e0bbf6e346313cc42f08b8d08562e6b8dccdda56
B. Bueno <afterglow9000@gmail.com>
2020-03-02 00:09:23 -0700

    How To Make An Android / iOS Mobile Game In Unity Tutorial - Part 009 - Main Menu

---
+++ Assets/Scripts/PlayOrQuit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayOrQuit : MonoBehaviour
+{
+    void OnGUI()
+    {
+        float height1 = Screen.height / 2 + 100;
+        float width1 = Screen.width / 2 - 50;
+
+        Rect rectangle1 = new Rect(width1, height1, 100, 30);
+
+        if (GUI.Button(rectangle1, "Play Again"))
+        {
+            SceneManager.LoadScene(2);
+        }
+
+        float height2 = Screen.height / 2 + 150;
+        float width2 = Screen.width / 2 - 50;
+
+        Rect rectangle2 = new Rect(width2, height2, 100, 30);
+
+        if (GUI.Button(rectangle2, "Main Menu"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
